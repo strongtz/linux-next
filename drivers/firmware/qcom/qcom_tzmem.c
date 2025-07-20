4fe0c2cef6f2a3c9c484a3e649a00fb4f0ac2b80
Stephan Gerhold <stephan.gerhold@linaro.org>
2025-07-20 16:15:54 +0200

    wip: firmware: qcom: tzmem: Use "self owner" bits when running in EL2
    It seems like the SHM bridge needs to use a different set of bits (the
    "self owner" bits) when running as the hypervisor in EL2.
    
    Make the SHM bridge (and SCM calls in general) work properly in EL2 by
    porting a similar change from the msm-5.4 vendor kernel:
    git.codelinaro.org/clo/la/kernel/msm-5.4/-/commit/9cb1271ce620ab7cdd53f00c9d951004c9713254
    
    TODO: This should be likely changed to a DT property, since usage of
    is_hyp_mode_available() is discouraged in drivers.
    
    Signed-off-by: Stephan Gerhold <stephan.gerhold@linaro.org>

--- drivers/firmware/qcom/qcom_tzmem.c
+++ drivers/firmware/qcom/qcom_tzmem.c
@@ -71,6 +71,8 @@ static void qcom_tzmem_cleanup_area(struct qcom_tzmem_area *area)
 #include <linux/firmware/qcom/qcom_scm.h>
 #include <linux/of.h>
 
+#define QCOM_SHM_BRIDGE_SELF_OWNER BIT(1)
+#define QCOM_SHM_BRIDGE_SELF_OWNER_PERM_SHIFT 2
 #define QCOM_SHM_BRIDGE_NUM_VM_SHIFT 9
 
 static bool qcom_tzmem_using_shm_bridge;
@@ -113,6 +115,7 @@ notsupp:
 static int qcom_tzmem_init_area(struct qcom_tzmem_area *area)
 {
 	u64 pfn_and_ns_perm, ipfn_and_s_perm, size_and_flags;
+	u64 ns_vmids = QCOM_SCM_VMID_HLOS;
 	int ret;
 
 	if (!qcom_tzmem_using_shm_bridge)
@@ -122,12 +125,18 @@ static int qcom_tzmem_init_area(struct qcom_tzmem_area *area)
 	ipfn_and_s_perm = (u64)area->paddr | QCOM_SCM_PERM_RW;
 	size_and_flags = area->size | (1 << QCOM_SHM_BRIDGE_NUM_VM_SHIFT);
 
+	if (is_hyp_mode_available()) {
+		size_and_flags = area->size | QCOM_SHM_BRIDGE_SELF_OWNER |
+				 (QCOM_SCM_PERM_RW << QCOM_SHM_BRIDGE_SELF_OWNER_PERM_SHIFT);
+		ns_vmids = 0;
+	}
+
 	u64 *handle __free(kfree) = kzalloc(sizeof(*handle), GFP_KERNEL);
 	if (!handle)
 		return -ENOMEM;
 
 	ret = qcom_scm_shm_bridge_create(pfn_and_ns_perm, ipfn_and_s_perm,
-					 size_and_flags, QCOM_SCM_VMID_HLOS,
+					 size_and_flags, ns_vmids,
 					 handle);
 	if (ret)
 		return ret;
